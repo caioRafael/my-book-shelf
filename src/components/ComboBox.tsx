c437497b7a81226bcac0ba7f5ef6e8251a06cb52
caioRafael <caiorafaelrg@gmail.com>
2023-04-06 13:01:52 -0300

    combobox

--- src/components/ComboBox.tsx
+++ src/components/ComboBox.tsx
@@ -1,23 +1,31 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 import { ReactNode } from 'react'
 
+interface IComboboxOptions {
+  key: string | number
+  text: string
+}
 interface ComboBoxProps {
   children: ReactNode
+  options: IComboboxOptions[]
+  onChange: (item: string) => void
 }
 
 export function ComboBox(props: ComboBoxProps) {
-  const { children } = props
+  const { children, options, onChange } = props
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>{children}</DropdownMenu.Trigger>
       <DropdownMenu.Portal>
         <DropdownMenu.Content className="flex flex-col gap-2 p-2 w-44 max-h-60 border border-secondaryText bg-white rounded-lg mt-2">
-          <DropdownMenu.Item>item 1</DropdownMenu.Item>
-          <DropdownMenu.Item>item 1</DropdownMenu.Item>
-          <DropdownMenu.Item>item 1</DropdownMenu.Item>
-          <DropdownMenu.Item>item 1</DropdownMenu.Item>
-          <DropdownMenu.Item>item 1</DropdownMenu.Item>
-          <DropdownMenu.Item>item 1</DropdownMenu.Item>
+          {options?.map((item) => (
+            <DropdownMenu.Item
+              key={item.key}
+              onClick={() => onChange(item.text as string)}
+            >
+              {item.text}
+            </DropdownMenu.Item>
+          ))}
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,20 +1,47 @@
 'use client'
 import * as Toolbar from '@radix-ui/react-toolbar'
 import { ComboBox } from './ComboBox'
+import { ChevronDown } from 'lucide-react'
+import { useState } from 'react'
 
 export function SearchBar() {
+  const [selected, setSelected] = useState<string>('All')
   return (
-    <Toolbar.Root className="w-1/3 h-12 border rounded-full border-secondaryText">
-      <ComboBox>
-        <Toolbar.Button className="h-full w-[20%] rounded-l-full">
-          All
+    <Toolbar.Root className="w-1/3 h-12 border rounded-full border-secondaryText flex">
+      <ComboBox options={option} onChange={setSelected}>
+        <Toolbar.Button className="h-full w-[20%] rounded-l-full flex items-center justify-around">
+          {selected}
+          <ChevronDown size={20} />
         </Toolbar.Button>
       </ComboBox>
       <input
         type="text"
         placeholder="Search"
-        className="h-full w-[80%] rounded-r-full"
+        className="h-full w-[80%] rounded-r-full p-2"
       />
     </Toolbar.Root>
   )
 }
+
+const option = [
+  {
+    key: 'all',
+    text: 'All',
+  },
+  {
+    key: 'title',
+    text: 'Title',
+  },
+  {
+    key: 'autor',
+    text: 'Autor',
+  },
+  {
+    key: 'text',
+    text: 'Text',
+  },
+  {
+    key: 'subjects',
+    text: 'Subjects',
+  },
+]
